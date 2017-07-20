d9934bc1d7e9311527012581887ed7ca0738d17c
cian <cianswork@gmail.com>
2017-07-20 13:47:22 +0800

    add transform struct to .h

--- include/slamBase.h
+++ include/slamBase.h
@@ -58,6 +58,13 @@ struct RESULT_OF_PNP
     int inliers;
 };
 
+struct Transform
+{
+   Matrix<double,3,3>R;
+   Matrix<double,3,1>T;
+   
+};
+
 // 函数接口
 // image2PonitCloud 将rgb图转换为点云
 PointCloud::Ptr image2PointCloud( cv::Mat& rgb, cv::Mat& depth, CAMERA_INTRINSIC_PARAMETERS& camera );
